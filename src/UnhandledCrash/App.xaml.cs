a168fd9e43d52feef36ca17f4e07888e01d0fbae
Amogh Sarpotdar <amogh@teknova.in>
2025-11-23 01:52:59 +0530

    Unhandled Exception Treatment.

---
+++ src/UnhandledCrash/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace UnhandledCrash
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            DispatcherUnhandledException += OnUnhandledException;
+        }
+
+        void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Error encountered! Please contact support." + Environment.NewLine + e.Exception.Message);
+            Shutdown(1);
+            e.Handled = true;
+        }
+    }
+}
