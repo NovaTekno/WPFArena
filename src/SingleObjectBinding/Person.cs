53842066eee896a4773af478856c16c39fe580a8
Amogh Sarpotdar <amogh@teknova.in>
2025-11-23 03:21:45 +0530

    SetProperty<T> implementation example.

--- src/SingleObjectBinding/Person.cs
+++ src/SingleObjectBinding/Person.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SingleObjectBinding;
 
@@ -13,11 +15,19 @@ public class Person : ObservableObject
         get { return _age; }
         set
         {
-            if (_age != value)
-            {
-                _age = value;
-                OnPropertyChanged("Age");
-            }
+            SetProperty(ref _age, value);
+            OnPropertyChanged("Age");
+        }
+    }
+
+    protected void SetProperty<T>(ref T field, T value, [CallerMemberName] string propName = null)
+    {
+        if (!EqualityComparer<T>.Default.Equals(field, value))
+        {
+            field = value;
+            var pc = PropertyChanged;
+            if (pc != null)
+                pc(this, new PropertyChangedEventArgs(propName));
         }
     }
 
